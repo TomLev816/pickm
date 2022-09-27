23e91e3f70a2fbe27685fded6d44ff3134193435
TomLev816 <thomaslever508@gmail.com>
2022-09-27 17:11:45 -0400

    add week selector key

--- src/components/weekselecter.tsx
+++ src/components/weekselecter.tsx
@@ -13,6 +13,7 @@ const WeekSelecterCard: React.FC<{
         className={active ? "relative block py-1.5 px-3 border-0 bg-blue-600 outline-none transition-all duration-300 rounded text-white hover:text-white hover:bg-blue-600 shadow-md focus:shadow-md" : "relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 text-gray-800 hover:text-gray-800 hover:bg-gray-400 hover:bg-opacity-40"}
         name={weekName}
         onClick={() => onWeekChange(weekNum)}
+        key={weekNum}
         >
         {weekName}
       </button>
